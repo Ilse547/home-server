c38de0a5c227ad01dc422222e2811ffe25bc43f4
Lihuel <lihuel@debian>
2025-06-13 01:53:06 +0100

    file preview on the homepage

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express, { response } from "express"
 import {logger} from './middleware/logger.js'
 import multer from "multer";
-import path from "path";
+import path, { resolve } from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
 
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
 const PORT = 3000
 const storage = multer.diskStorage({
@@ -20,17 +25,45 @@ app.set("view engine", "ejs");
 app.use(logger)
 
 app.get('/', (request, response) => {
-  response.render('index')
+  const uploadDir = path.join(__dirname, "uploads")
+  fs.readdir(uploadDir, (error, files) => {
+    if(error){
+      console.error("error reading the directory: ", error)
+      return response.status(500).send("unanle to load diles.");
+    }
+      response.render('index', {files })
+  })
 })
 
 app.get('/upload', (request, response) =>{
   response.render('upload')
 })
 app.post("/up", upload.single("myFile"), (request, response) => {
-  if(!request.file){
+
+  const file = request.file
+  const newFileName = request.body.newFileName
+
+  if(!file){
     return response.status(400).send("no file uploaded :(");
   }
+
+  const uploadDir = path.join(__dirname, "uploads")
+  const newFilePath = newFileName
+    ? path.join(uploadDir, `${newFileName}${path.extname(file.originalname)}`) // Use the new name with the original extension
+    : file.path; // Keep the original name if no new name is provided
+  if(newFileName){
+    fs.rename(file.path, newFilePath, (error) =>{
+      if(error){
+        console.error("error renaming file: ", error)
+        return response.status(500).send("error renamign file.")
+      }
+      response.redirect("/")
+    })
+  } else{
     response.redirect("/")
+  }
+
+
 })
 
 app.listen(PORT, () => {
