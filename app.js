da203f0c14bf04c17754188e84edbf71ea8528da
Lihuel <lihuel@debian>
2025-06-13 01:31:35 +0100

    Add file uploads

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-import express from "express"
+import express, { response } from "express"
 import {logger} from './middleware/logger.js'
+import multer from "multer";
+import path from "path";
 
-
-const app =express()
+const app = express()
 const PORT = 3000
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+});
+
+const upload = multer({storage: storage});
+
+
 app.use(express.static('public'))
 app.set("views", "./views");
 app.set("view engine", "ejs");
@@ -11,6 +21,16 @@ app.use(logger)
 
 app.get('/', (request, response) => {
   response.render('index')
+})
+
+app.get('/upload', (request, response) =>{
+  response.render('upload')
+})
+app.post("/up", upload.single("myFile"), (request, response) => {
+  if(!request.file){
+    return response.status(400).send("no file uploaded :(");
+  }
+    response.redirect("/")
 })
 
 app.listen(PORT, () => {
